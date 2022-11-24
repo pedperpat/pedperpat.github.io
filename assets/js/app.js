034117037b322e0daf9b2a2abe24f9788681aa14
Pedro Antonio Pérez Paterna <pedro-aperez1@hotmail.com>
2022-11-24 12:02:12 +0000

    Edit experience v1

--- assets/js/app.js
+++ assets/js/app.js
@@ -139,9 +139,9 @@ let footer = $(`
                 <h6 class="display">Get in Touch</h6>
               </div>
                 <form name="form1" action="https://formcarry.com/s/6FdJ3sRTN" method="POST" accept-charset="UTF-8" >
-                  <input id="name" type="text" name="name" placeholder="Your Name" required/>
-                  <input id="email" type="email" name="email" placeholder="Email Address" required/>                  
-                  <textarea id="textArea" name="message" placeholder="Type your Message" required></textarea>
+                  <input id="name" type="text" name="name" placeholder="Tu nombre" required/>
+                  <input id="email" type="email" name="email" placeholder="Dirección de correo" required/>                  
+                  <textarea id="textArea" name="message" placeholder="Escribe tu mensaje" required></textarea>
               
                   <div id="main">
                     <button id="lnch" type="button" value="Send" >Send</button>
