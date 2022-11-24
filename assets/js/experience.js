ded161dc7cfcfde507cded663b0f03fa86420b47
Pedro Antonio Pérez Paterna <pedro-aperez1@hotmail.com>
2022-11-24 12:17:20 +0000

    Experiencia

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -7,23 +7,23 @@ const exp = [
   {
     title: "Desarrollador Junior Business Central y Android",
     cardImage: "https://media-exp1.licdn.com/dms/image/C560BAQFhUk1_Iw0ReQ/company-logo_200_200/0/1519884618573?e=2147483647&v=beta&t=s82mlhTgl-UXCEOT4uo-6e41XRGfQFqvaJ_LG-zzl-s",
-    place: "IM Projects S",
+    place: "IM Projects",
     time: "(Abril, 2021 - actualidad)",
-    desp: "<li>Desarrollando extensiones para Microsoft Business Central.</li> <li>Ayudando a crear y mejorar aplicaciones Android.</li> <li>He creado una aplicación para Android que se llama IM-Kitchen y desde la cual se puede visualizar toda la información de Ingredientes, Recetas y Platos en Microsoft Dynamics 365 Business Central tanto para operaciones de Backoffice como de planta.</li>",
+    desp: "<li>Desarrollo extensiones para Microsoft Business Central.</li> <li>Ayudo a crear y mejorar aplicaciones Android.</li> <li>He creado una aplicación para Android que se llama IM-Kitchen y desde la cual se puede visualizar toda la información de Ingredientes, Recetas y Platos en Microsoft Dynamics 365 Business Central tanto para operaciones de Backoffice como de planta.</li>",
   },
   {
     title: "Programador técnico",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    cardImage: "https://media-exp1.licdn.com/dms/image/C4D0BAQHMkah3IuGAmg/company-logo_200_200/0/1572871364295?e=2147483647&v=beta&t=vCt0zazhRzBnsf2g7r_V2dBeMd-Hp2v6DrvftycBE5k",
+    place: "Promociones Azarbe SA",
+    time: "(Julio, 2018 - Abril, 2021)",
+    desp: "<li>Desarrollo de soluciones para Microsoft Dynamics NAV</li><li>Trabajé en pequeños desarrollos para dotar de nuevas funciones al ERP Microsoft Dynamics NAV.</li><li>Gestión documental y de contabilidad.</li><li>Ofrecía ayuda y soporte ténico a usuarios.</li>",
   },
   {
     title: "Programador técnico",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    cardImage: "https://media-exp1.licdn.com/dms/image/C4E0BAQFuXso1f67LwQ/company-logo_200_200/0/1519892093522?e=2147483647&v=beta&t=aUAWqjT56ghrTxVhGqCXg3XdBDl2Izrbt4nl-WF47is",
+    place: "Datanet Corentia SL",
+    time: "(Julio, 2017 - Julio, 2018 )",
+    desp: "<li>Aprendí a desarrollar en Microsoft Dynamics NAV.</li><li>Desarrollos pequeños para clientes.</li>",
   },
 ];
 
@@ -55,7 +55,7 @@ const showCards2 = () => {
   );
   experiencecards.innerHTML = output;
 };
-document.addEventListener("DOMContentLoaded", showCards2);
+//document.addEventListener("DOMContentLoaded", showCards2);
 
 // Volunteership Cards
 
@@ -104,7 +104,7 @@ const showCards = () => {
   );
   volunteership.innerHTML = output;
 };
-document.addEventListener("DOMContentLoaded", showCards);
+//document.addEventListener("DOMContentLoaded", showCards);
 
 // Hackathon Section
 
@@ -222,4 +222,4 @@ const showCards3 = () => {
   );
   hackathonsection.innerHTML = output;
 };
-document.addEventListener("DOMContentLoaded", showCards3);
+//document.addEventListener("DOMContentLoaded", showCards3);
