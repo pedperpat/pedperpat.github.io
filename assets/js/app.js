cf979796813fd1a16aec1e80bc21355f290b7463
Pedro Antonio Pérez Paterna <pedro-aperez1@hotmail.com>
2022-11-24 12:49:57 +0000

    Edit card

--- assets/js/app.js
+++ assets/js/app.js
@@ -118,7 +118,7 @@ let footer = $(`
       <!--SVG code for quote-left icon-->
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
       </div>
-      <p class="tag" >Develop a passion for learning. If you do, you will never cease to grow.</p>
+      <p class="tag" >Desarrolla una pasión por aprender. Si lo haces, nunca dejarás de crecer.</p>
     </div>
 
     
@@ -136,7 +136,7 @@ let footer = $(`
         <div class="col-lg-6 col-md-12 mb-4 mb-md-0 form-comtainer">
           <div class="form-style-6">
              <div class="form-header">
-                <h6 class="display">Get in Touch</h6>
+                <h6 class="display">Estemos en contacto</h6>
               </div>
                 <form name="form1" action="https://formcarry.com/s/6FdJ3sRTN" method="POST" accept-charset="UTF-8" >
                   <input id="name" type="text" name="name" placeholder="Tu nombre" required/>
@@ -144,7 +144,7 @@ let footer = $(`
                   <textarea id="textArea" name="message" placeholder="Escribe tu mensaje" required></textarea>
               
                   <div id="main">
-                    <button id="lnch" type="button" value="Send" >Send</button>
+                    <button id="lnch" type="button" value="Send" >Enviar</button>
                     <div id="lnch_btn"><i class="fas fa-space-shuttle"></i></div>
                   </div>
                 </form>
